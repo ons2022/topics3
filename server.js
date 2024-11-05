5d86edac980c4ddad1a2d59d3569552bfe6fe175
ons2022 <onsismailhajrigmail.com>
2024-11-05 02:03:22 +0100

    first

--- server.js
+++ server.js
@@ -2,18 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
+const cors = require('cors');
+
+// Import Routes
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/user');
 const equipmentRoutes = require('./routes/equipment');
-const cors = require('cors');
+
+// Initialize Express App
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = 'mongodb+srv://ons:mAFAiazF4D5mP618@atlascluster.gskrewb.mongodb.net/safelife';
+
+// CORS Configuration
 app.use(cors({
-  origin: 'http://localhost:8100' // Replace with the origin of your Angular app
+  origin: 'http://localhost:8100',
 }));
-// Swagger setup
+
+// Swagger Setup
 const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
@@ -30,8 +37,8 @@ const swaggerOptions = {
     ],
     security: [
       {
-        bearerAuth: [] // Utilisation de l'authentification Bearer
-      }
+        bearerAuth: [],
+      },
     ],
     components: {
       securitySchemes: {
@@ -41,10 +48,10 @@ const swaggerOptions = {
           bearerFormat: 'JWT',
           name: 'Authorization',
           in: 'header',
-          description: 'Enter your JWT token'
-        }
-      }
-    }
+          description: 'Enter your JWT token',
+        },
+      },
+    },
   },
   apis: ['./routes/*.js'], // Path to the API docs
 };
@@ -61,8 +68,8 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/equipment', equipmentRoutes);
 
-// Database connection
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// Database Connection
+mongoose.connect(MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
   })
@@ -70,4 +77,16 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
     console.error('Error connecting to MongoDB:', err.message);
   });
 
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
+// 404 Handling
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+// Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
